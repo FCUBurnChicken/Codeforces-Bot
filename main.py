d13a168c589fa9971fbf2ec79afdb396efee166c
YJK0805 <jerryko103@gmail.com>
2023-07-06 17:11:04 +0800

    add get_user_info

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import settings
 import discord
+import requests
+import json
 from discord.ext import commands
 from discord import app_commands
 
 logger = settings.logging.getLogger('client')
+url = "https://codeforces.com/api"
+
+def get_user_info(handle):
+    response = requests.get(url + "/user.info?handles=" + handle)
+    response = json.loads(response.text)
+    return {
+        'handle': handle,
+        'rating': response['result'][0]['rating'],
+        'maxRating': response['result'][0]['maxRating'],
+        'rank': response['result'][0]['rank'],
+        'maxRank': response['result'][0]['maxRank']
+    }
 
 def main():
     intents = discord.Intents.all()
@@ -34,6 +48,12 @@ def main():
     async def reload(ctx, cogs):
         await client.reload_extension(f"cogs.{cogs}")
 
+    @client.command()
+    async def user(ctx, handle):
+        response = get_user_info(handle)
+        user_info = f"Handle: {response['handle']}\nRating: {response['rating']}\nMax Rating: {response['maxRating']}\nRank: {response['rank']}\nMax Rank: {response['maxRank']}"
+        await ctx.send(user_info)
+    
     client.run(settings.BOT_TOKEN, root_logger=True)
 
 if __name__ == '__main__':
